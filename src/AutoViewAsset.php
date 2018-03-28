ede4b1788d06bec7265f8e9ee9b28e4fd1d12b0f
Nick Tsai <myintaer@gmail.com>
2018-03-28 16:14:14 +0800

    Add file

---
+++ src/AutoViewAsset.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace app\assets;
+
+use Yii;
+
+/**
+ * Auto View Asset
+ * 
+ * @author  Nick Tsai <mtintaer@gmail.com>
+ * @version 1.0.0
+ * @example
+ *  Set $basePath = '@webroot/dist/app' for JS asset reference:
+ *  @app/views/site/index.php => @webroot/dist/app/site/index.js
+ *  @app/views/level/site/about.php => @webroot/dist/app/level/site/about.js
+ */
+class AutoViewAsset extends \yii\web\AssetBundle
+{
+    /**
+     * Asset base setting
+     *
+     * @var string Path with default related path `dist/app`
+     */
+    public $basePath = '@webroot/dist/app';
+    public $baseUrl = '@web/dist/app';
+    
+    /**
+     * Asset initialization
+     *
+     * @return void
+     */
+    public function init()
+    {
+        parent::init();
+
+        // Fetch View name
+        $view = Yii::$app->controller->getView();
+        $viewFile = $view->getViewFile();
+        $viewFile = str_replace(Yii::$app->controller->getViewPath() . DIRECTORY_SEPARATOR, '', $viewFile);
+        $viewName = str_replace("." . $view->defaultExtension, '', $viewFile);
+
+        // Get full controller name (route)
+        $controller = Yii::$app->controller->id;
+
+        // Compose to a asset file name without extension
+        $fileName = $controller . DIRECTORY_SEPARATOR . $viewName;
+
+        // Define asset files with adding extension
+        $jsFile = "{$fileName}.js";
+        $cssFile = "{$fileName}.css";
+
+        // Load asset files
+        if (file_exists($this->baseUrl.$jsFile)) {
+            $this->js[] = $file;
+        }
+        if (file_exists($this->baseUrl.$cssFile)) {
+            $this->css[] = $file;
+        }
+    }
+}
+
